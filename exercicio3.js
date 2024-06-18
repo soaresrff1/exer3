a74dcbf33ed983146c35d5f2663587aad537e0ab
Rafael Soares <rafaelsoaresyyb1@gmail.com>
2024-06-18 08:47:22 -0300

    feat: retirado os numeros duplicados

--- exercicio3.js
+++ exercicio3.js
@@ -1,4 +1,4 @@
-let array = [1, 6, 3, 8, 7, 8, 10, 14, 15, 11];
+let array = [1, 6, 3, 8, 7, 8, 10, 14, 15, 11, 15];
 
 for (let i = 0; i < array.length; i++) {
   for (let j = i + 1; j < array.length; j++) {
@@ -6,6 +6,16 @@ for (let i = 0; i < array.length; i++) {
       let aux = array[i];
       array[i] = array[j];
       array[j] = aux;
+    }
+  }
+}
+console.log(array);
+
+for (let i = 0; i < array.length; i++) {
+  for (let j = i + 1; j < array.length; j++) {
+    if (array[i] == array[j]) {
+      array.splice(j, 1);
+      j--;
     }
   }
 }
