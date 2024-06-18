3d1ecfc0b2a8a684bfd28e09cef842c815faac64
Rafael Soares <rafaelsoaresyyb1@gmail.com>
2024-06-18 08:41:47 -0300

    init: ordenado o array

--- exercicio3.js
+++ exercicio3.js
@@ -1,5 +1,12 @@
-let array = [1,3,6,8,7,8,10,14,15,11]
+let array = [1, 6, 3, 8, 7, 8, 10, 14, 15, 11];
 
-
-
-
+for (let i = 0; i < array.length; i++) {
+  for (let j = i + 1; j < array.length; j++) {
+    if (array[i] > array[j]) {
+      let aux = array[i];
+      array[i] = array[j];
+      array[j] = aux;
+    }
+  }
+}
+console.log(array);
