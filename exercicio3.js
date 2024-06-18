94bb2dfe6e1ac32433d79cd8a77b518bc470f001
Rafael Soares <rafaelsoaresyyb1@gmail.com>
2024-06-18 08:51:38 -0300

    refactor: diminuindo o tamanho do codigo, para ter um desenvolvimento mais rapido e pratico

--- exercicio3.js
+++ exercicio3.js
@@ -6,14 +6,7 @@ for (let i = 0; i < array.length; i++) {
       let aux = array[i];
       array[i] = array[j];
       array[j] = aux;
-    }
-  }
-}
-console.log(array);
-
-for (let i = 0; i < array.length; i++) {
-  for (let j = i + 1; j < array.length; j++) {
-    if (array[i] == array[j]) {
+    } else if (array[i] == array[j]) {
       array.splice(j, 1);
       j--;
     }
